90ffb300554b56639e080703be5c4a41a4558e76
Winstonfg <winston.grant@protonmail.com>
2022-01-17 11:04:26 -0800

    added a power up
    allows the power up to be destroyed on pickup

--- Project4/Assets/Scripts/PlayerMovement.cs
+++ Project4/Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class PlayerMovement : MonoBehaviour
 {
     //Setting up vars
     public float speed = 10;
     public float forwardMovement;
-    
+    public bool hasPowerUP;
+
     //since we are using the .addforce we need to set up to use component rigibody
     public Rigidbody playerRB;
-    
+
     //getting the focal point GO so we can make moving forward the way the camera is facing
     public GameObject focalPoint;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,14 +38,22 @@ public class PlayerMovement : MonoBehaviour
 
         //pushes the player in the direction of the comera facing
         playerRB.AddForce(focalPoint.transform.forward * forwardMovement * speed);
-       
+
     }
+
 	private void OnTriggerEnter(Collider other)
 	{
+		if (other.CompareTag("PowerUp"))
+		{
+            Destroy(other.gameObject);
+            hasPowerUP = true;
+		}
 		
 	}
+
 	private void OnCollisionEnter(Collision collision)
 	{
 		
 	}
+
 }
